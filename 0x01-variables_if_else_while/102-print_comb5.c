c0adf0de98b60ccc51508046f38d881edd831c1e
Ay-iman <ayomiderabiu00@gmail.com>
2022-12-27 16:56:45 -0800

    low prog

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -2,28 +2,32 @@
 
 /**
  * main - prints all possible combinations of two two-digit numbers
+ * using putchar
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int i, j;
-	for (i = 0; i < 100; j++)
+
+	for (i = 0; i < 100; i++)
 	{
-		if (i < j)
+		for (j = 0; j < 100; j++)
 		{
-			putchar((i / 10) + 48);
-			putchar((i % 10) + 48);
-			putchar(' ');
-			putchar((j / 10) + 48);
-			putchar((j % 10) + 48);
-			if (i != 98 || j != 99)
+			if (i < j)
 			{
-				putchar(',');
+				putchar((i / 10) + 48);
+				putchar((i % 10) + 48);
 				putchar(' ');
+				putchar((j / 10) + 48);
+				putchar((j % 10) + 48);
+				if (i != 98 || j != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
-}
-putchar("\n");
-return (0);
+	putchar('\n');
+	return (0);
 }
