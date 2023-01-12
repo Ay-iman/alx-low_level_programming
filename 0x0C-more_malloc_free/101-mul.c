d1290f8280eb507bb6a0db73b48f343906673554
Ay-iman <ayomiderabiu00@gmail.com>
2023-01-12 06:00:03 -0800

    D

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,29 +1,65 @@
-#include <stdio.h>
 #include "main.h"
 #include <stdlib.h>
-#include <ctype.h>
 
 /**
-*main - multiplies two numbers
-*@argc: arguement count
-*@argv: arguement vector
-*Return: '0' on success
-*/
-int main(int argc, char *argv[])
-{
-	int ans;
+ * _print - moves a string one place to the left and prints the string
+ * @str: string to move
+ * @l: size of string
+ *
+ * Return: void
+ */
 
-	if (argc != 3)
+void _print(char *str, int l)
+{
+	int i, j;
+
+	i = j = 0;
+	while (i < l)
 	{
-		printf("Error\n");
-		exit(98);
+		if (str[i] != '0')
+			j = 1;
+		if (j || i == l - 1)
+			_putchar(str[i]);
+		i++;
 	}
-	if (!(isdigit(argv[1])) || !(isdigit(argv[2])))
+
+	_putchar('\n');
+	free(str);
+}
+
+/**
+ * mul - multiplies a char with a string and places the answer into dest
+ * @n: char to multiply
+ * @num: string to multiply
+ * @num_index: last non NULL index of num
+ * @dest: destination of multiplication
+ * @dest_index: highest index to start addition
+ *
+ * Return: pointer to dest, or NULL on failure
+ */
+
+char *mul(char n, char *num, int num_index, char *dest, int dest_index)
+{
+	int j, k, mul, mulrem, add, addrem;
+
+	mulrem = addrem = 0;
+	for (j = num_index, k = dest_index; j >= 0; j--, k--)
 	{
-		printf("Error\n");
-		exit(98);
+		mul = (n - '0') * (num[j] - '0') + mulrem;
+		mulrem = mul / 10;
+		add = (dest[k] - '0') + (mul % 10) + addrem;
+		addrem = add / 10;
+		dest[k] = add % 10 + '0';
 	}
-	ans = (atoi(argv[1]) * atoi(argv[2]));
-	printf("%d\n", ans);
-	return (0);
+	for (addrem += mulrem; k >= 0 && addrem; k--)
+	{
+		add = (dest[k] - '0') + addrem;
+		addrem = add / 10;
+		dest[k] = add % 10 + '0';
+	}
+	if (addrem)
+	{
+		return (NULL);
+	}
+	return (dest);
 }
